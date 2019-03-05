86327499599b63bfc0063539f2716a8622db67ae
NeoWu1216 <neo1216.wu@gmail.com>
2019-03-05 03:36:18 -0600

    Updates

---
+++ precache-manifest.312037ff9e685d75c8e01b668208c18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87ed84012ea39c338209b6c2a5b7c16",
+    "url": "/backpack-ing/static/media/backpack.c87ed840.jpg"
+  },
+  {
+    "revision": "43f80332ed1db687a8c4",
+    "url": "/backpack-ing/static/js/runtime~main.43f80332.js"
+  },
+  {
+    "revision": "94566a422233ea028397",
+    "url": "/backpack-ing/static/js/main.94566a42.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/backpack-ing/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "94566a422233ea028397",
+    "url": "/backpack-ing/static/css/main.26217455.chunk.css"
+  },
+  {
+    "revision": "4f260358aab9a1497c7ebe7bc2d09623",
+    "url": "/backpack-ing/index.html"
+  }
+];
