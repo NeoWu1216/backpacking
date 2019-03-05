ed23a15c1aa9347c265c9fda9f118cef0cdb1359
NeoWu1216 <neo1216.wu@gmail.com>
2019-03-04 19:58:42 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Homepage from './components/homepage.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<Homepage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './main.css'
+
+const Homepage = () => {
+  return (
+    <div class="banner" id="main-img">
+      <h1> Backpacking </h1>
+      <h3> Your ultimate dream</h3>
+      <button type="button" class="btn btn-secondary btn-lg"> 
+        Starts here 
+      </button>
+    </div>
+  )
+}
+
+export default Homepage
