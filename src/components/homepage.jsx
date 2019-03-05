83a7744fbeee087a73015733e7cb88a01b5f35b7
NeoWu1216 <neo1216.wu@gmail.com>
2019-03-05 03:14:04 -0600

    Update content

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './main.css'
 
 const Homepage = () => {
+  const backgroundImage = "linear-gradient(135deg, rgba(158, 46, 124, 0.1) 0%,"+
+  "rgba(90, 199, 85, 0.15) 50%), url("+require("../assets/backpack.jpg")+")";
   return (
-    <div class="banner" id="main-img">
+    <div style={{backgroundImage}} class="banner" id="main-img">
       <h1> Backpacking </h1>
       <h3> Your ultimate dream</h3>
       <button type="button" class="btn btn-secondary btn-lg"> 
